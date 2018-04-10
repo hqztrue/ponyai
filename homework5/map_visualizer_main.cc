7dd140b11cadbf34a48747ba11a4ce2a6975a79a
hqztrue <hqz@megvii.com>
2018-04-10 15:06:44 +0800

    finish hw5 q2

--- homework5/map_visualizer_main.cc
+++ homework5/map_visualizer_main.cc
@@ -8,43 +8,45 @@
 
 DEFINE_string(route_file_path, "", "Path of displayed route");
 
-bool point_equal(const Point3D &p1, const Point3D &p2){
+bool point_equal(const interface::geometry::Point3D &p1, const interface::geometry::Point3D &p2){
 	const double eps = 1e-3;
 	return fabs(p1.x()-p2.x())<eps && fabs(p1.y()-p2.y())<eps && fabs(p1.z()-p2.z())<eps;
 }
 
 void find_pred_succ(){
 	homework5::map::MapLib map_lib;
-	interface::map::Map map = map_lib->map_proto();
+	interface::map::Map map = map_lib.map_proto();
 	int n = map.lane_size();
 	for (int i=0;i<n;++i){
-		interface::map::Lane &lane1 = map.lane(i);
+		//interface::map::Lane &lane1 = map.lane(i);
 		for (int j=0;j<n;++j)
 			if (i!=j){
-				interface::map::Lane &lane2 = map.lane(j);
+				//interface::map::Lane &lane2 = map.lane(j);
 				//check lane(i)->lane(j), by central_line
-				if (point_equal(lane1.central_line().point(lane1.central_line().point_size()-1),lane2.central_line().point(0))){
-						interface::map::Id *pid = lane1.add_successor();
-						pid->set_id(lane2.id());
-						pid = lane2.add_predecessor();
-						pid->set_id(lane1.id());
+				if (point_equal(map.lane(i).central_line().point(map.lane(i).central_line().point_size()-1),map.lane(j).central_line().point(0))){
+						interface::map::Id *pid = map.mutable_lane(i)->add_successor();
+						*pid = map.lane(j).id();
+						pid = map.mutable_lane(j)->add_predecessor();
+						*pid = map.lane(i).id();
 					}
 			}
 	}
-	WriteProtoToTextFile(map, "processed_map_proto.txt");
+	CHECK(file::WriteProtoToTextFile(map, "/home/hqz/ponyai/homework5/processed_map_proto.txt"));
 }
 
 int main(int argc, char* argv[]) {
   google::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
+  
+  find_pred_succ(); puts("pred_succ"); return 0;
 
+  
   QApplication app(argc, argv);
   QCoreApplication::setOrganizationName("pony.ai");
   QCoreApplication::setOrganizationDomain("pony.ai");
   QCoreApplication::setApplicationName("MapVisualizer");
 
   homework5::MainWindow main_window(nullptr);
-  //find_pred_succ(); return 0;
   
   if (!FLAGS_route_file_path.empty()) {
     interface::route::Route route;
