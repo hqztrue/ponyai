b8f260ea822eb808026f10de1ef2df80a55794d8
hqztrue <hqztrue@sina.com>
2018-04-28 19:37:48 +0800

    .

--- homework6/agents/hqztrue/hqztrue_agent.h
+++ homework6/agents/hqztrue/hqztrue_agent.h
@@ -27,19 +27,19 @@ class FrogVehicleAgent : public simulation::VehicleAgent {
       const interface::agent::AgentStatus& agent_status) {
     const double max_velocity = 10, eps = 1e-5;
     interface::control::ControlCommand command;
-    double dist = len(agent_status.vehicle_status().position(), agent_status.route_status().destination());
+    double dist = CalcDistance(agent_status.vehicle_status().position(), agent_status.route_status().destination());
 	
 	if (control > 1.0+eps)exit(0);
 	if (acceleration){
 		command.set_throttle_ratio(control);
 		prev_control = control;
-		if (CalcVelocity(agent_status.vehicle_status().velocity())>=max_velocity)acceleration = false;
+		if (len(agent_status.vehicle_status().velocity())>=max_velocity)acceleration = false;
 	}
 	else {
 		command.set_brake_ratio(control);
 		prev_control = -control;
-		if (CalcVelocity(agent_status.vehicle_status().velocity())<=0.1){
-			control += delta;
+		if (len(agent_status.vehicle_status().velocity())<=0.1){
+			control += control_delta;
 			acceleration = true;
 		}
 	}
@@ -47,7 +47,7 @@ class FrogVehicleAgent : public simulation::VehicleAgent {
 	
 	if (!first_run){
 		FILE *f = fopen("/home/hqz/ponyai/homework6/table.txt", "a");
-		fprintf(f, "%.6lf %.6lf %.6lf\n",prev_status.velocity(), prev_control, agent_status.vehicle_status().acceleration_vcs());
+		fprintf(f, "%.6lf %.6lf %.6lf\n",prev_status.vehicle_status().velocity(), prev_control, agent_status.vehicle_status().acceleration_vcs());
 		fclose(f);
 	}
 	first_run = false;
@@ -58,7 +58,7 @@ class FrogVehicleAgent : public simulation::VehicleAgent {
   /*virtual interface::control::ControlCommand RunOneIteration(
       const interface::agent::AgentStatus& agent_status) {
     interface::control::ControlCommand command;
-    double dist = len(agent_status.vehicle_status().position(), agent_status.route_status().destination());
+    double dist = CalcDistance(agent_status.vehicle_status().position(), agent_status.route_status().destination());
 	
 	
 	
