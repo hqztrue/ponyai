a85358cb7fca86470259c83c7f3dc3b354ec81f3
hqztrue <hqztrue@sina.com>
2018-06-08 21:45:48 +0800

    perception

--- perception/perception_evaluator.cc
+++ perception/perception_evaluator.cc
@@ -105,9 +105,9 @@ PerceptionEvaluationResult PerceptionEvaluator::RunEvaluation() {
     // Load camera image if there is a corresponding one.
 	int frameID = atoi(file::path::FilenameStem(pointcloud_file).c_str());
 	int p = pointcloud_file.find("select");
-	string prefix = pointcloud_file(0, p-1);
+	std::string prefix = pointcloud_file(0, p-1);
 	p = prefix.rfind("/");
-	string video_name = prefix.substr(p+1);
+	std::string video_name = prefix.substr(p+1);
 	printf("split %s %s %d\n", pointcloud_file.c_str(), video_name.c_str(), frameID);
     const std::string image_file =
         file::path::Join(options_.camera_folder,
