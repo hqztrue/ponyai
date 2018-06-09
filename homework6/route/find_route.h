588100c060f4273447cb7aec8668388822d2e4d3
hqztrue <hqztrue@sina.com>
2018-06-09 13:42:13 +0800

    .

--- homework6/route/find_route.h
+++ homework6/route/find_route.h
@@ -3,7 +3,7 @@
 #include "glog/logging.h"
 #include "common/utils/file/file.h"
 #include "gflags/gflags.h"
-//#include "homework5/display/main_window.h"
+#include "homework6/display/main_window.h"
 #include "homework6/map/map_lib.h"
 #include "homework5/geometry/geometry.h"
 //#include<string>
