0baf50f4a9a6be2a61c629939036237e7f61bf53
hqztrue <hqztrue@sina.com>
2018-06-09 13:36:36 +0800

    .

--- homework6/agents/agents.cc
+++ homework6/agents/agents.cc
@@ -2,7 +2,7 @@
 
 #include "homework6/agents/agents.h"
 
-//#include "homework6/agents/sample/sample_agent.h"
+#include "homework6/agents/sample/sample_agent.h"
 #include "homework6/agents/hqztrue/hqztrue_agent.h"
 
 // Register sample vehicle agent to a factory with its type name "sample_agent"
---
+++ homework6/route/find_route.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "glog/logging.h"
+#include "common/utils/file/file.h"
+#include "gflags/gflags.h"
+//#include "homework5/display/main_window.h"
+#include "homework6/map/map_lib.h"
+#include "homework5/geometry/geometry.h"
+//#include<string>
+//using namespace std;
+
+void find_pred_succ();
+void find_route(interface::route::Route &route);
+void find_route(char path_src[], char path_dst[]);
+
+
+
+
