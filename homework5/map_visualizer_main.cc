d6cd03aaf8cab52eac2618d8d72083810836e7b2
hqztrue <hqztrue@sina.com>
2018-06-16 20:02:35 +0800

    central line

--- homework5/map_visualizer_main.cc
+++ homework5/map_visualizer_main.cc
@@ -15,13 +15,16 @@ int main(int argc, char* argv[]) {
   google::InitGoogleLogging(argv[0]);
   
   //find_pred_succ(); puts("pred_succ"); return 0;
-  /*for (int i=1;i<=5;++i){
+  
+  /*if (argc==1){
+  for (int i=1;i<=5;++i){
 	  char path_src[305], path_dst[305];
 	  sprintf(path_src, "/home/hqztrue/Desktop/ponyai/homework5/data/routes/route_request_%d.txt", i);
 	  sprintf(path_dst, "/home/hqztrue/Desktop/ponyai/homework5/data/routes/route_result_%d.txt", i);
 	  find_route(path_src, path_dst);
   }
-  puts("find_route"); return 0;*/
+  puts("find_route"); return 0;
+  */
   
   QApplication app(argc, argv);
   QCoreApplication::setOrganizationName("pony.ai");
